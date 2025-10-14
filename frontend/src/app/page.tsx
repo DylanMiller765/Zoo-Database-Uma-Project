66ace084d2d21843b38de011507084199368b6a5
Charles M <muuurbles@gmail.com>
2025-10-14 15:31:12 -0500

    Added a header to main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      {/* TODO: Add home page content */}
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold text-center my-8 text-green-800">
+        If you're seeing this congrats!
+      </h1>
     </main>
-  )
+  );
 }
